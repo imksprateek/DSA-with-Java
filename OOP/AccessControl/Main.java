75359dd22f635f840e6ec2d4716ca1205cca9d27
imksprateek <ksprateek2004@gmail.com>
2024-04-02 22:45:12 +0530

    Access Control file added

--- OOP/AccessControl/Main.java
+++ OOP/AccessControl/Main.java
@@ -1,5 +1,11 @@
 package oops.AccessControl;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import oops.polymorphism.Circle;
+import oops.polymorphism.Shapes;
+
+import java.util.Scanner;
+
 public class Main {
     /*
             Class       Package      Subclass          Subclass        World
@@ -11,4 +17,16 @@ private       +
 
 
      */
+
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+
+        //instanceof operator
+        System.out.println(circle instanceof Shapes);
+        System.out.println(circle instanceof Circle);
+
+        //getClass method
+        System.out.println(circle.getClass());
+        System.out.println(circle.getClass().getName());
+    }
 }
