0d1d0ad68c3ed42fc90f511b849b167938a628da
imksprateek <ksprateek2004@gmail.com>
2024-08-06 19:28:55 +0530

    Started with Doubly Linked List

--- Concepts/DataStructures/LinkedList/DoublyLinkedList/CustomDoublyLinkedList.java
+++ Concepts/DataStructures/LinkedList/DoublyLinkedList/CustomDoublyLinkedList.java
@@ -1,4 +1,32 @@
 package Concepts.DataStructures.LinkedList.DoublyLinkedList;
 
+import Concepts.DataStructures.LinkedList.SinglyLinkedList.CustomLinkedList;
+
 public class CustomDoublyLinkedList {
+    private Node head;
+    private Node tail;
+    private int size;
+
+    void CustomLinkedList(){
+        this.size = 0;
+    }
+
+    public void insertNode(int val){
+
+    }
+
+    private class Node{
+        private int value;
+        private Node next;
+        private Node prev;
+
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
 }
--- Concepts/DataStructures/LinkedList/DoublyLinkedList/Main.java
+++ Concepts/DataStructures/LinkedList/DoublyLinkedList/Main.java
@@ -1,4 +1,6 @@
 package Concepts.DataStructures.LinkedList.DoublyLinkedList;
 
 public class Main {
+    CustomDoublyLinkedList list = new CustomDoublyLinkedList();
+
 }
