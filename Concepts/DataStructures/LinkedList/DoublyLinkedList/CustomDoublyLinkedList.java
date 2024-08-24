56e3c764f36d4265ee946682555c595944d6f6ac
imksprateek <ksprateek2004@gmail.com>
2024-08-24 08:50:48 +0530

    Added doubly Linked List files

--- Concepts/DataStructures/LinkedList/DoublyLinkedList/CustomDoublyLinkedList.java
+++ Concepts/DataStructures/LinkedList/DoublyLinkedList/CustomDoublyLinkedList.java
@@ -11,8 +11,105 @@ public class CustomDoublyLinkedList {
         this.size = 0;
     }
 
-    public void insertNode(int val){
+    public void insertFirst(int val){
+        Node newNode = new Node(val);
+        newNode.next = head;
+        newNode.prev = null;
 
+        if(tail == null){
+            tail = newNode;
+        }
+
+        if(head != null){
+            head.prev = newNode;
+        }
+        head = newNode;
+
+        size++;
+    }
+
+    public void insertLast(int val){
+        Node newNode = new Node(val);
+        if(head == null){
+            head = newNode;
+        }
+
+        if(tail != null){
+            tail.next = newNode;
+            newNode.prev = tail;
+        }
+        tail = newNode;
+
+        size++;
+    }
+
+    public void insertAtPosition(int pos, int val){
+
+        if(pos < 0 || pos > size){
+            System.out.println("Invalid position");
+        }else if(pos == 0){
+            insertFirst(val);
+        }else if(pos == size){
+            System.out.println("size is + " + size);
+            insertLast(val);
+        }else {
+            Node newNode = head;
+            for (int i = 1; i < pos; i++) {
+                newNode = newNode.next;
+            }
+            Node tempNode = new Node(val);
+            if(newNode.next != null){
+                newNode.next.prev = tempNode;
+            }
+            tempNode.next = newNode.next;
+            newNode.next = tempNode;
+            tempNode.prev = newNode;
+            size++;
+        }
+    }
+
+    public void insertAfter(int target, int val){
+        Node node = head;
+
+        while(node.value != target){
+            node = node.next;
+            if(node == null){
+                System.out.println("Target element not found");
+                return;
+            }
+        }
+
+        Node tempNode = new Node(val);
+        tempNode.next = node.next;
+        node.next.prev = tempNode;
+        node.next = tempNode;
+        tempNode.prev = node;
+
+        size++;
+    }
+
+    public void displayListForward(){
+        System.out.println("Printing in forward direction....");
+        Node current = head;
+        System.out.print("START -> ");
+        while(current != null){
+            System.out.print(current.value + " -> ");
+            current = current.next;
+        }
+        System.out.print(" END ");
+        System.out.println();
+    }
+
+    public void displayListBackward(){
+        System.out.println("Printing in backward direction....");
+        Node current = tail;
+        System.out.print(" END <- ");
+        while(current != null){
+            System.out.print(current.value + " <- ");
+            current = current.prev;
+        }
+        System.out.print(" START ");
+        System.out.println();
     }
 
     private class Node{
--- Concepts/DataStructures/LinkedList/DoublyLinkedList/Main.java
+++ Concepts/DataStructures/LinkedList/DoublyLinkedList/Main.java
@@ -1,6 +1,20 @@
 package Concepts.DataStructures.LinkedList.DoublyLinkedList;
 
 public class Main {
-    CustomDoublyLinkedList list = new CustomDoublyLinkedList();
+    public static void main(String[] args) {
+        CustomDoublyLinkedList list = new CustomDoublyLinkedList();
 
+        list.insertFirst(12);
+        list.insertFirst(13);
+        list.insertFirst(14);
+
+        list.insertFirst(15);
+        list.insertLast(11);
+
+        list.insertAtPosition(4, 99);
+        list.insertAfter(14, 77);
+
+        list.displayListForward();
+        list.displayListBackward();
+    }
 }
