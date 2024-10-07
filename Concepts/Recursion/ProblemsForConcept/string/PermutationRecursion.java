a1424c6b75e2e6ce3046439064b1d1514f3dc5b2
imksprateek <ksprateek2004@gmail.com>
2024-10-07 19:23:46 +0530

    Added permutation and combinations with recursion file

--- Concepts/Recursion/ProblemsForConcept/string/PermutationRecursion.java
+++ Concepts/Recursion/ProblemsForConcept/string/PermutationRecursion.java
@@ -6,6 +6,8 @@ import java.util.List;
 public class PermutationRecursion {
     public static void main(String[] args) {
         System.out.println(permutate("", "abc").toString());
+        System.out.println(permutate("", "abcd").toString());
+        System.out.println(countPermutations("", "abcd"));
     }
 
     public static List<String> permutate(String processed, String unprocessed){
@@ -24,5 +26,19 @@ public class PermutationRecursion {
         }
 
         return result;
+    }
+
+    public static int countPermutations(String processed, String unprocessed){
+        if(unprocessed.isEmpty()){
+            return 1;
+        }
+        int count = 0;
+        char ch = unprocessed.charAt(0);
+        for(int i=0; i<unprocessed.length();i++){
+            String start = unprocessed.substring(0,i);
+            String end = unprocessed.substring(i,unprocessed.length());
+            count = count + countPermutations(start + ch + end, unprocessed.substring(1));
+        }
+        return count;
     }
 }
