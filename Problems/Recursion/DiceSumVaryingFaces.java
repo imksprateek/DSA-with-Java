a372c625b3a7a7d6ead66ae25cc8e75c617de9b3
imksprateek <ksprateek2004@gmail.com>
2024-10-09 22:59:26 +0530

    Modified DiceSum problem to work with varying number of faces

--- Problems/Recursion/DiceSumVaryingFaces.java
+++ Problems/Recursion/DiceSumVaryingFaces.java
@@ -1,24 +1,23 @@
 package Problems.Recursion;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
-public class DiceSum {
+public class DiceSumVaryingFaces {
     public static void main(String[] args) {
-        System.out.println(combinations("", 4));
+        System.out.println(combinations("", 4, 6));
     }
 
-    public static List<String> combinations(String processed, int sum){
+    public static List<String> combinations(String processed, int sum, int faces){
         List<String> result = new ArrayList<>();
 
         if(sum==0){
             result.add(processed);
             return result;
         }
-        for (int i = 1; i <= 6; i++) {
+        for (int i = 1; i <= faces; i++) {
             if(i<=sum) {
-                result.addAll(combinations(processed + i, sum - i));
+                result.addAll(combinations(processed + i, sum - i, faces));
             }
         }
         return result;
