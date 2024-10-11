ba27815fe07bd0bf59ed08f91b455609c4b4af43
imksprateek <ksprateek2004@gmail.com>
2024-10-11 22:08:50 +0530

    Modified to include diagonal movement

--- Concepts/Recursion/ProblemsForConcept/backtracking/PrintPathsInMaze.java
+++ Concepts/Recursion/ProblemsForConcept/backtracking/PrintPathsInMaze.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class PrintPathsInMaze {
     public static void main(String[] args) {
         System.out.println(getPaths("",3, 3));
+        System.out.println(getPathsIncludeDiagonal("", 3,3));
     }
 
     public static List<String> getPaths(String processed, int row, int col){
@@ -22,6 +23,26 @@ public class PrintPathsInMaze {
             result.addAll(getPaths(processed + "R", row, col-1));
         }else if(row > 1){
             result.addAll(getPaths(processed+"D", row-1, col));
+        }
+
+        return result;
+    }
+
+    public static List<String> getPathsIncludeDiagonal(String processed, int row, int col){
+        List<String> result = new ArrayList<>();
+
+        if(row == 1 && col == 1){
+            result.add(processed);
+            return result;
+        }
+        if(row > 1 && col > 1){
+            result.addAll(getPathsIncludeDiagonal(processed + "D", row-1, col));
+            result.addAll(getPathsIncludeDiagonal(processed+"R", row, col-1));
+            result.addAll(getPathsIncludeDiagonal(processed + "*", row-1, col-1));
+        }else if(col > 1){
+            result.addAll(getPathsIncludeDiagonal(processed + "R", row, col-1));
+        }else if(row > 1){
+            result.addAll(getPathsIncludeDiagonal(processed+"D", row-1, col));
         }
 
         return result;
