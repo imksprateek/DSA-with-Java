a731c7277e773dd82f379ef6661c003fafd0cd87
imksprateek <ksprateek2004@gmail.com>
2024-10-20 17:10:20 +0530

    minor fixes

--- Problems/LinkedList/FindCycleInLinkedList_LeetCode141.java
+++ Problems/LinkedList/FindCycleInLinkedList_LeetCode141.java
@@ -3,17 +3,17 @@ import java.util.LinkedList;
 
 public class FindCycleInLinkedList_LeetCode141 {
     public static void main(String[] args) {
-        ListNode head = new ListNode(1);
-        head.next = new ListNode(2);
-        head.next.next = new ListNode(3);
-        head.next.next.next = new ListNode(4);
-        head.next.next.next.next = new ListNode(5);
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
         head.next.next.next.next.next = head.next;
 
         System.out.println("The number of cycles is " + hasCycle(head));
     }
 
-    public static int hasCycle(ListNode head) {
+    public static int hasCycle(Node head) {
         boolean  cycleFound = false;
 
         if(head == null){
@@ -23,8 +23,8 @@ public class FindCycleInLinkedList_LeetCode141 {
             return 0;
         }
 
-        ListNode slowPointer = head;
-        ListNode fastPointer = head;
+        Node slowPointer = head;
+        Node fastPointer = head;
 
         //Find if a cycle exists
         while(fastPointer != null && fastPointer.next != null){
@@ -50,16 +50,16 @@ public class FindCycleInLinkedList_LeetCode141 {
     }
 
 
-    public static class ListNode {
+    public static class Node {
         public int val;
-        public ListNode next;
+        public Node next;
 
-        public ListNode(int x) {
+        public Node(int x) {
             this.val = x;
             this.next = null;
         }
 
-        public ListNode(int val, ListNode next) {
+        public Node(int val, Node next) {
             this.val = val;
             this.next = next;
         }
