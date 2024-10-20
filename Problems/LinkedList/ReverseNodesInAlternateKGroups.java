52aab8c95232ecf0cdf8f0dd6731f48bdb7dbb76
imksprateek <ksprateek2004@gmail.com>
2024-10-20 18:14:24 +0530

    Reversing alternate k node groups

---
+++ Problems/LinkedList/ReverseNodesInAlternateKGroups.java
@@ -0,0 +1,86 @@
+package Problems.LinkedList;
+
+import Concepts.DataStructures.LinkedList.SinglyLinkedList.Node;
+
+public class ReverseNodesInAlternateKGroups {
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(3);
+        head.next.next = new Node(5);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(2);
+
+        Node newHead = reverseAltKGroup(head, 2);
+        printList(newHead);
+    }
+
+    public static Node reverseAltKGroup(Node head, int k) {
+        if(head == null || head.next == null || k<=1){
+            return head;
+        }
+
+        Node prev = null;
+        Node curr = head;
+        Node next = curr.next;
+
+        while(curr != null && canProceed(curr,k)){
+            Node last = prev;
+            Node newEnd = curr;
+
+            for(int i=0; curr != null && i<k; i++){
+                curr.next = prev;
+                prev = curr;
+                curr = next;
+
+                if(next != null){
+                    next = next.next;
+                }
+            }
+
+            if(last != null){
+                last.next = prev;
+            }else{
+                head = prev;
+            }
+
+            newEnd.next = curr;
+
+            //Skipping k nodes after reversing every time
+            for(int i=0; curr != null && i<k; i++){
+                prev = curr;
+                curr = curr.next;
+            }
+        }
+
+        return head;
+    }
+
+    public static boolean canProceed(Node head, int k){
+        if(head == null){
+            return false;
+        }
+        int count = 0;
+        Node temp = head;
+
+        while(temp != null && count != k){
+            temp = temp.next;
+            count++;
+        }
+
+        if(count == k){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static void printList(Node head){
+        while(head != null){
+            System.out.print(head.value + " -> ");
+            head = head.next;
+        }
+        System.out.print("null ");
+        System.out.println();
+    }
+}
