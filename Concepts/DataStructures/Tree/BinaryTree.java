6de8107bd395732e16783da86f6968460393219a
imksprateek <ksprateek2004@gmail.com>
2025-01-10 18:36:35 +0530

    Added functions for traversals - preorder, inorder and postorder

--- Concepts/DataStructures/Tree/BinaryTree.java
+++ Concepts/DataStructures/Tree/BinaryTree.java
@@ -75,6 +75,42 @@ public class BinaryTree {
         prettyDisplay(node.left, level+1);
     }
 
+    public void preOrder(){
+        preOrder(root);
+    }
+    private void preOrder(Node node){
+        if(node == null){
+            return;
+        }
+        System.out.print(node.value + ", ");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    public void inOrder(){
+        inOrder(root);
+    }
+    private void inOrder(Node node){
+        if(node == null){
+            return;
+        }
+        inOrder(node.left);
+        System.out.print(node.value + ", ");
+        inOrder(node.right);
+    }
+
+    public void postOrder(){
+        postOrder(root);
+    }
+    private void postOrder(Node node){
+        if(node == null){
+            return;
+        }
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.print(node.value + ", ");
+    }
+
     private static class Node{
         int value;
         Node left;
--- Concepts/DataStructures/Tree/Main.java
+++ Concepts/DataStructures/Tree/Main.java
@@ -15,5 +15,18 @@ public class Main {
         //polulateSorted function makes sure the tree is not skewed
         bst.populateSorted(new int[]{9,10,11,12,13,14,15});
         bst.display();
+
+
+        /*
+        Binary Tree Traversals:
+        - Preorder: Root, Left, Right
+        - Inorder: Left, Root, Right
+        - Postorder: Left, Right, Root
+
+        Usages:
+        - Preorder: Used to create a copy of the tree.
+        - Inorder: Used to get elements of a Binary Search Tree in increasing order.
+        - Postorder: Used to delete any node from the Tree and performing any bottom=up calculation like calculating the height. Takes care of children before deleting the parent node.
+         */
     }
 }
