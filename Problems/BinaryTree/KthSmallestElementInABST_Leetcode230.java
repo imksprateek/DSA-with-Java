46efa852265e49cd82d7acc30e7cd22f110ec9c2
imksprateek <ksprateek2004@gmail.com>
2025-03-14 18:04:16 +0530

    Added queue based approach to finding kth smallest element in a BST

--- Problems/BinaryTree/KthSmallestElementInABST_Leetcode230.java
+++ Problems/BinaryTree/KthSmallestElementInABST_Leetcode230.java
@@ -2,6 +2,9 @@ package Problems.BinaryTree;
 
 import Concepts.DataStructures.Tree.BinaryTree.Node;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class KthSmallestElementInABST_Leetcode230 {
     static int count = 0, val = 0;
     public static void main(String[] args) {
@@ -13,6 +16,7 @@ public class KthSmallestElementInABST_Leetcode230 {
         System.out.println(kthSmallest(root, 2));
     }
 
+    //Most optimal solution using two global int variables
     public static int kthSmallest(Node root, int k) {
         inOrder(root, k);
         return val;
@@ -29,5 +33,29 @@ public class KthSmallestElementInABST_Leetcode230 {
             return;
         }
         inOrder(node.right, k);
+    }
+
+
+    //Solution using Queue - Not the most optimal solution
+    public int kthSmallest2(Node root, int k) {
+        Queue<Integer> q = new LinkedList();
+        inOrder(root, k, q);
+
+        int val = 0;
+
+        for(int i=0; i<k; i++){
+            val = (int) q.poll();
+        }
+
+        return val;
+    }
+
+    public void inOrder2(Node node, int k, Queue<Integer> q){
+        if(node==null){
+            return;
+        }
+        inOrder2(node.left, k, q);
+        q.add(node.value);
+        inOrder2(node.right, k, q);
     }
 }
